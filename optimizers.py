84fbd505b757c01fd6df7db59dcdc9c65506aeae
ADulian <duli95@gmail.com>
2021-10-22 10:32:27 +0100

    Init Commit

--- optimizers.py
+++ optimizers.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class SGD:
-    def __init__(self, learning_rate=1.0, decay=0.):
+    def __init__(self, learning_rate=1.0, decay=0., momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
+        self.momentum = momentum
         self.iterations = 0
 
     def pre_update_params(self):
@@ -12,8 +13,26 @@ class SGD:
             self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
 
     def update_params(self, layer):
-        layer.weights += -self.current_learning_rate * layer.dweights
-        layer.biases += -self.current_learning_rate * layer.dbiases
+        if self.momentum:
+            # If there is no momentum weights, create an empty array
+            if not hasattr(layer, 'weight_momentum'):
+                layer.weight_momentum = np.zeros_like(layer.weights)
+                layer.bias_momentum = np.zeros_like(layer.biases)
+
+            # Weight updates with momentum
+            weight_updates = self.momentum * layer.weight_momentum - self.current_learning_rate * layer.dweights
+            bias_updates = self.momentum * layer.bias_momentum - self.current_learning_rate * layer.dbiases
+
+            # Update momentum
+            layer.weight_momentum = weight_updates
+            layer.bias_momentum = bias_updates
+
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
