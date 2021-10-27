3c19d9de55c859d44969c0e1a50a2180008b9c48
Albert Dulian <Albert.Dulian@vicon.com>
2021-10-27 18:29:09 +0100

    Init_Commit

--- activations.py
+++ activations.py
@@ -4,7 +4,7 @@ class ReLU:
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.maximum(0, inputs)
-        
+
     def backward(self, dvalues):
         # Zero gradient where input values were negative
         self.dinputs = dvalues.copy()
@@ -13,25 +13,33 @@ class ReLU:
 class SoftMax:
     def forward(self, inputs):
         self.inputs = inputs
-        
+
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        
+
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
         self.output = probabilities
-        
+
     def backward(self, dvalues):
         # Create uninitialized array
         self.dinputs = np.empty_like(dvalues)
-        
+
         # Enumerate outputs and gradients
         for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             # Flatten output array
             single_output = single_output.reshape(-1,1)
-            
+
             # Jacobian
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
-            
+
             # Calculate sample-wise gradient
             # and add it to the array of sample gradients
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+class Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * (1 - self.output) * self.output
